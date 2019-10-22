cc6207bd693f4343ac01a059bd1967e4fd21eded
fakeharxy <harkerp@gmail.com>
2019-10-22 09:44:01 +0100

    refactor PostcodeChecker tests

--- spec/helpers/postcode_checker_spec.rb
+++ spec/helpers/postcode_checker_spec.rb
@@ -2,20 +2,28 @@ require_relative '../../helpers/postcode_checker'
 
 RSpec.describe PostcodeChecker do
   it 'returns true if the postcode fetcher returns Lambeth' do
-    fetcher = double('fetcher', fetch: { 'result': { 'lsoa': 'Lambeth 005C'} })
+    fetcher = double_maker('Lambeth')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(true)
   end
 
   it 'returns true if the postcode fetcher returns Southwark' do
-    fetcher = double('fetcher', fetch: { 'result': { 'lsoa': 'Southwark 005C'} })
+    fetcher = double_maker('Southwark')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(true)
   end
 
   it 'returns false if the postcode fetcher returns Camden' do
-    fetcher = double('fetcher', fetch: { 'result': { 'lsoa': 'Camden 005C'} })
+    fetcher = double_maker('Camden')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(false)
   end
+end
+
+def double_maker(location)
+  double('fetcher',
+         fetch: {
+           'result':
+           { 'lsoa': "#{location} 005C" }
+         })
 end
