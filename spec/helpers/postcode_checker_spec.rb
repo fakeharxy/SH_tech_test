4e55eacfd9687f38467325387b99ed1033b25d79
fakeharxy <harkerp@gmail.com>
2019-10-22 12:20:04 +0100

    test refactor

--- spec/helpers/postcode_checker_spec.rb
+++ spec/helpers/postcode_checker_spec.rb
@@ -2,19 +2,19 @@ require_relative '../../helpers/postcode_checker'
 
 RSpec.describe PostcodeChecker do
   it 'returns true if the postcode fetcher returns Lambeth' do
-    fetcher = double_maker('Lambeth')
+    fetcher = valid_double_maker('Lambeth')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(true)
   end
 
   it 'returns true if the postcode fetcher returns Southwark' do
-    fetcher = double_maker('Southwark')
+    fetcher = valid_double_maker('Southwark')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(true)
   end
 
   it 'returns false if the postcode fetcher returns Camden' do
-    fetcher = double_maker('Camden')
+    fetcher = valid_double_maker('Camden')
     checker = PostcodeChecker.new('SW90LR', fetcher)
     expect(checker.whitelisted?).to be(false)
   end
@@ -30,7 +30,9 @@ RSpec.describe PostcodeChecker do
   end
 end
 
-def double_maker(location)
+private
+
+def valid_double_maker(location)
   double('fetcher',
          fetch: {
            'result' =>
